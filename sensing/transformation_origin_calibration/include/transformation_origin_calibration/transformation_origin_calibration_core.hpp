06487e0cac4029b6944775a323849b9e7ba77134
Volt <volt.autonomous@gmail.com>
2022-08-11 16:52:30 +0300

    add transformation_origin_calibration package

---
+++ sensing/transformation_origin_calibration/include/transformation_origin_calibration/transformation_origin_calibration_core.hpp
@@ -0,0 +1,34 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GNSS_POSER__GNSS_POSER_CORE_HPP_
+#define GNSS_POSER__GNSS_POSER_CORE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+namespace transformation_origin_calibration
+{
+class TransformationOriginCalibration : public rclcpp::Node
+{
+public:
+  explicit TransformationOriginCalibration(const rclcpp::NodeOptions & node_options);
+
+private:
+  rclcpp::Publisher<geometry_msgs::msg::TransformStamped>::SharedPtr pub_transformation_origin_calibration_;
+  geometry_msgs::msg::TransformStamped transformation_origin_calibration_;
+};
+}  // namespace gnss_poser
+
+#endif  // GNSS_POSER__GNSS_POSER_CORE_HPP_
---
+++ sensing/transformation_origin_calibration/src/transformation_origin_calibration_core.cpp
@@ -0,0 +1,34 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "transformation_origin_calibration/transformation_origin_calibration_core.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace transformation_origin_calibration
+{
+    TransformationOriginCalibration::TransformationOriginCalibration(const rclcpp::NodeOptions & node_options)
+: rclcpp::Node("transformation_origin_calibration", node_options)
+{
+  pub_transformation_origin_calibration_ = this->create_publisher<geometry_msgs::msg::TransformStamped>(
+    "transformation_origin_calibration", rclcpp::QoS{1});
+}
+
+}  // namespace transformation_origin_calibration
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(transformation_origin_calibration::TransformationOriginCalibration)
