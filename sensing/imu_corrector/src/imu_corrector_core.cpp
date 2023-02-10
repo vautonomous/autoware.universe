064e42b4617a7dd64dde677b93963100288d7ba1
volt autonomous <volt.autonomous@gmail.com>
2023-02-10 18:17:21 +0300

    fix imu corrector qos

--- sensing/imu_corrector/src/imu_corrector_core.cpp
+++ sensing/imu_corrector/src/imu_corrector_core.cpp
@@ -28,7 +28,7 @@ ImuCorrector::ImuCorrector(const rclcpp::NodeOptions & node_options)
   angular_velocity_stddev_zz_ = declare_parameter<double>("angular_velocity_stddev_zz", 0.03);
 
   imu_sub_ = create_subscription<sensor_msgs::msg::Imu>(
-    "input", rclcpp::QoS{1}, std::bind(&ImuCorrector::callbackImu, this, std::placeholders::_1));
+    "input", rclcpp::SensorDataQoS(), std::bind(&ImuCorrector::callbackImu, this, std::placeholders::_1));
 
   imu_pub_ = create_publisher<sensor_msgs::msg::Imu>("output", rclcpp::QoS{10});
 }
